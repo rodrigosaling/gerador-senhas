76f932ba3da35eb56d03684a303d53a256c38f78
Rodrigo Saling <rodrigosaling@gmail.com>
2023-10-12 10:23:42 -0300

    adicionando uma explicação na página inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Gerador de Senhas</h1>
+
+<p>
+	Baseado no <a href="https://1password.com/pt/password-generator/"
+		>gerador de senhas do 1Password</a
+	>, este aqui é um simples gerador de senhas "fáceis de memorizar" que usa palavras em português,
+	ao invés de palavras em inglês igual à referência.
+</p>
