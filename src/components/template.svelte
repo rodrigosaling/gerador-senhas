1a9b127f5c6e5efdf5f442c0910ca9bced3967ae
Rodrigo Saling <rodrigosaling@gmail.com>
2024-01-18 21:27:36 -0300

    removes unused component and commented code

--- src/components/template.svelte
+++ src/components/template.svelte
@@ -40,7 +40,5 @@
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
 			'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
 			'Noto Color Emoji';
-		/* background-color: #e7e5e4;
-		color: #292524; */
 	}
 </style>
